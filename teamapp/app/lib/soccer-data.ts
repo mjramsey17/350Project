9d72706867f8b965b331c1b56038fc141c0eb2a1
Sheen <srtoliver@mail.roanoke.edu>
2024-11-15 14:59:45 -0500

    Progress

---
+++ teamapp/app/lib/soccer-data.ts
@@ -0,0 +1,354 @@
+const Events = [
+    // Game VSs Catholic
+    {
+        EventId: 1,
+        // GameId: ,
+        Time: 38,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId: 
+    },
+    {
+        EventId: 2,
+        // GameId: ,
+        Time: 38,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId: 
+    },
+    {
+        EventId: 2,
+        // GameId: ,
+        Time: 38,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 3,
+        // GameId: ,
+        Time: 46,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 4,
+        // GameId: ,
+        Time: 56,
+        Type: 'Goal',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 5,
+        // GameId: ,
+        Time: 73,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 6,
+        // GameId: ,
+        Time: 78,
+        Type: 'Goal',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 7,
+        // GameId: ,
+        Time: 78,
+        Type: 'Assist',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 8,
+        // GameId: ,
+        Time: 23,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 9,
+        // GameId: ,
+        Time: 54,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 10,
+        // GameId: ,
+        Time: 56,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 11,
+        // GameId: ,
+        Time: 56,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 12,
+        // GameId: ,
+        Time: 80,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 13,
+        // GameId: ,
+        Time: 83,
+        Type: 'Red',
+        Team: 1,
+        // PlayerId:
+    },
+    // Game 2 VS Oglethorpe
+    {
+        EventId: 14,
+        // GameId: ,
+        Time: 33,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 15,
+        // GameId: ,
+        Time: 34,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 16,
+        // GameId: ,
+        Time: 41,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 17,
+        // GameId: ,
+        Time: 56,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 18,
+        // GameId: ,
+        Time: 62,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 19,
+        // GameId: ,
+        Time: 72,
+        Type: 'Red',
+        Team: 1,
+        // PlayerId:
+    },
+    // Game 3 VS Emory
+    {
+        EventId: 20,
+        // GameId: ,
+        Time: 65,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 21,
+        // GameId: ,
+        Time: 65,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 22,
+        // GameId: ,
+        Time: 65,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 23,
+        // GameId: ,
+        Time: 20,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 24,
+        // GameId: ,
+        Time: 42,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 25,
+        // GameId: ,
+        Time: 68,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 26,
+        // GameId: ,
+        Time: 73,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    // Game 4 VS Marietta
+    {
+        EventId: 27,
+        // GameId: ,
+        Time: 40,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 28,
+        // GameId: ,
+        Time: 82,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 29,
+        // GameId: ,
+        Time: 89,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    // Game 5 VS Centre
+    {
+        EventId: 30,
+        // GameId: ,
+        Time: 43,
+        Type: 'Goal',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 31,
+        // GameId: ,
+        Time: 43,
+        Type: 'Assist',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 32,
+        // GameId: ,
+        Time: 48,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 33,
+        // GameId: ,
+        Time: 48,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 34,
+        // GameId: ,
+        Time: 53,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 35,
+        // GameId: ,
+        Time: 53,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 36,
+        // GameId: ,
+        Time: 69,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 37,
+        // GameId: ,
+        Time: 89,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 38,
+        // GameId: ,
+        Time: 89,
+        Type: 'Goal',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 38,
+        // GameId: ,
+        Time: 89,
+        Type: 'Assist',
+        Team: 1,
+        // PlayerId:
+    },
+    {
+        EventId: 39,
+        // GameId: ,
+        Time: 21,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    {
+        EventId: 40,
+        // GameId: ,
+        Time: 89,
+        Type: 'Yellow',
+        Team: 0,
+        // PlayerId:
+    },
+    // Game 6 VS Lynchburg
+    {
+        EventId: 41,
+        // GameId: ,
+        Time: 20,
+        Type: 'Yellow',
+        Team: 1,
+        // PlayerId:
+    }
+]
+
+export {Events}
